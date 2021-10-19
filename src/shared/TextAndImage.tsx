e523a3f4a83e6824e13d15a9ff3ae109181d5d71
bisbisz <90863881+bisbisz@users.noreply.github.com>
2021-10-18 22:53:09 -0700

    Update TextAndImage.tsx

--- src/shared/TextAndImage.tsx
+++ src/shared/TextAndImage.tsx
@@ -18,7 +18,7 @@ const TextAndImage = ({reversed, image, text}: {reversed?: boolean, image: strin
       <p>{text}</p> 
     </div>
     <div>
-      <img  style={{width: '400px'}} src={image}/>
+      <img style={{width: '400px'}} src={https://i.ibb.co/J3DGsbr/King-Ape.png} alt="Crazy Ape NFT"/>
     </div>
   </div>
   )
